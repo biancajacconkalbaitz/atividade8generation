cefc7147ae41f09c7664c57432fad7ba87a804e1
Bianca Jaccon Kalbaitz <biancakalbaitz@gmail.com>
2023-09-13 07:43:49 -0300

    Adição de Classes Pessoa Física e Pessoa Jurídica

--- atividade8exercicio1/src/atividade8exercicio1/pessoafisica.java
+++ atividade8exercicio1/src/atividade8exercicio1/pessoafisica.java
@@ -1,5 +1,40 @@
 package atividade8exercicio1;
 
-public class pessoafisica {
+public abstract class pessoafisica extends Cliente {
+	
+		private static final String String = null;
+		String CPF;
+		
+		public abstract void Cliente(String email, String nomecompleto, char sexo, int idade, float altura, float peso, String CPF); {
+			
+			float peso;
+			float idade;
+			int altura;
+			char sexo;
+			String nomecompleto;
+			String email;
+			
+			super (email, nomecompleto, sexo, idade, altura, peso, CPF);
+		
+			
+			this.CPF = CPF;
+		}
+		public String getCPF() {
+			return CPF;
+		}
 
-}
+		public void setCPF(String CPF) {
+			this.CPF = CPF;
+		}
+
+		
+		public void visualizar() {
+			super.visualizar();
+			System.out.println("CADASTRO PESSOA FÍSICA, EX: XXX.XXX.XXX-XX " + this.CPF);
+		}
+	}
+		
+
+
+
+
